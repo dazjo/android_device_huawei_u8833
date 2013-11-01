795392eabba98471e1f25a1a3bda0bbebf0ab0f8
Daz Jones <yuki@thebronasium.com>
2013-11-01 17:46:54 +0000

    u8833: basic configuration

---
+++ BoardConfig.mk
@@ -0,0 +1,61 @@
+#
+# Copyright 2013 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from the proprietary version
+-include vendor/huawei/u8833/BoardConfigVendor.mk
+
+# Inherit from the common msm7x27a definitions
+-include device/huawei/msm7x27a-common/BoardConfigCommon.mk
+
+# Architecture and CPU
+TARGET_CPU_SMP := true
+
+TARGET_BOOTLOADER_BOARD_NAME := u8833
+TARGET_OTA_ASSERT_DEVICE := u8833,hwu8833,u8951,hwu8951
+
+# Kernel
+TARGET_KERNEL_CONFIG := u8833_defconfig
+
+# These currently have to go to the ramdisk for wlan_detect to pick them up.
+# Hopefully they can join their friends at $(KERNEL_MODULES_OUT) soon. :(
+KERNEL_EXTERNAL_MODULES:
+	mkdir -p $(TARGET_ROOT_OUT)/wifi
+	rm -rf $(TARGET_OUT_INTERMEDIATES)/ath6kl-huawei
+	cp -a hardware/atheros/wifi/ath6kl-huawei $(TARGET_OUT_INTERMEDIATES)/
+	$(MAKE) -C $(TARGET_OUT_INTERMEDIATES)/ath6kl-huawei/cfg80211 KERNEL_OUT=$(KERNEL_OUT) ARCH="arm" CROSS_COMPILE="arm-eabi-" modules
+	$(MAKE) -C $(TARGET_OUT_INTERMEDIATES)/ath6kl-huawei/ar6000 KERNEL_OUT=$(KERNEL_OUT) ARCH="arm" CROSS_COMPILE="arm-eabi-" modules
+	$(TARGET_OBJCOPY) --strip-unneeded $(TARGET_OUT_INTERMEDIATES)/ath6kl-huawei/cfg80211/cfg80211.ko $(TARGET_ROOT_OUT)/wifi/cfg80211.ko
+	$(TARGET_OBJCOPY) --strip-unneeded $(TARGET_OUT_INTERMEDIATES)/ath6kl-huawei/ar6000/ar6000.ko $(TARGET_ROOT_OUT)/wifi/ar6000.ko
+
+TARGET_KERNEL_MODULES := KERNEL_EXTERNAL_MODULES
+
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1073741824 # 0x40000000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1207943168 # 0x47FFC000
+
+# Wi-Fi
+BOARD_HAS_ATH_WLAN := true
+BOARD_WLAN_DEVICE := ath6kl
+
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_$(BOARD_WLAN_DEVICE)
+
+WIFI_DRIVER_FW_PATH_AP := "ap"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WIFI_DRIVER_FW_PATH_P2P := "p2p"
+
+WIFI_DRIVER_MODULE_NAME := "ar6000"
+WIFI_DRIVER_MODULE_PATH := "/data/misc/wifi/load/ar6000.ko"
